992700fdd3fa56eae5ff5657db4657e70cdb0025
efarin <efarin@efarin.42.fr>
2022-05-19 18:33:27 +0200

    ca marche en fait

--- srcs/requirements/wordpress/start.sh
+++ srcs/requirements/wordpress/start.sh
@@ -3,17 +3,6 @@ cd /var/www/html/wordpress
 wp config create --dbname=wordpress --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=database:3306
 wp db create
 wp core install --url=localhost --title="WP-CLI" --admin_user=wpcli --admin_password=wpcli --admin_email=info@wp-cli.org --path=/var/www/html/wordpress
-#wp core install --url=wpclidemo.dev --title="WP-CLI" --admin_user=wpcli --admin_password=wpcli --admin_email=info@wp-cli.org --path=/var/www/html/wordpress
-# wp core install --url=efarin.42.fr --title=Adetu --admin_user=root --admin_password=root --admin_email=patate@frite.fr --skip-email --path=/var/www/html/wordpress
-# wp user create user1 hiougo@hotmail.fr --path=/var/www/html/wordpress
+# wp user create user1 efarin@hotmail.fr --path=/var/www/html/wordpress
 wp plugin update --all
 exec php-fpm7 -F -R
-# if [ ! /var/www/html/wordpress/wpclidemo.dev/wp-config.php ] ; then
-# 	wp core download --path=/var/www/html/wordpress
-# 	cd /var/www/html/wordpress
-# 	wp config create --dbname=wordpress --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=database:3306
-# 	wp db create
-# 	wp core install --url=wpclidemo.dev --title="WP-CLI" --admin_user=wpcli --admin_password=wpcli --admin_email=info@wp-cli.org
-# 	wp plugin update --all
-# fi
-# exec php-fpm7 -F -R
